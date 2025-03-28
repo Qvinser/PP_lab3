fd5dff757068dd47a0180abe8365b47c6b7a72c3
Qvinser <ribnayastaya777@gmail.com>
2025-03-28 12:14:27 +0700

    upd

--- PP_lab3/Source.cpp
+++ PP_lab3/Source.cpp
@@ -18,7 +18,7 @@ int main() {
 		b[i] = i;
 	// Директива OpenMP для распараллеливания цикла
 	auto start = std::chrono::steady_clock::now();
-//#pragma omp parallel for
+#pragma omp parallel for
 	for (int i = 0; i < 100; i++)
 	{
 		a[i] = f(b[i]);
@@ -26,7 +26,7 @@ int main() {
 	}
 	int result = 0;
 	// Далее значения a[i] и b[i] используются, например, так:
-//#pragma omp parallel for reduction(+ : result)
+#pragma omp parallel for reduction(+ : result)
 	for (int i = 0; i < 100; i++)
 		result += (a[i] + b[i]);
 	std::chrono::duration<double> elapsed = std::chrono::steady_clock::now() - start;
