c65d664bcf1819908f0941c0c1c143d358e4fa3f
Qvinser <ribnayastaya777@gmail.com>
2025-04-11 00:53:19 +0700

    upd

--- PP_lab3_Jacobi/PP_lab3_Jacobi.cpp
+++ PP_lab3_Jacobi/PP_lab3_Jacobi.cpp
@@ -123,7 +123,7 @@ void generateRHS(int n, double*& F) {
 int main()
 {
     double** Matrix, * b, * y, * x;
-    int n = 4000;
+    int n = 5000;
     int thread_num = 1;
     omp_set_num_threads(thread_num);
     setlocale(0, "");
